e6b3b7856020381a22e08204f18ee47c347993e4
Mirza Younus Baig <mirzayounus40@gmail.com>
2023-03-10 03:17:13 -0500

    Updated output tests

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
-# # 
+# Basic Input
 # x = input()
 # x = input("Test Input Prompt ")
 # x = int(input("Test Input Integer Prompt"))
@@ -7,5 +7,16 @@
 # x, y = [int(i) for i in input().split()]
 # x, y = [int(i) for i in input().split")]
 
+# Basic Output
 # print(x)
 # print(x, y)
+# print(x, y, sep = "-")
+
+# print(f"{x}")
+# print("{}".format(x))
+# print("{} {}".format(y, x))
+# print("{1} {0}".format(x, y))
+# print("{} {}".format(x, y), sep = "$$", end = " Done\n")
+# print(" {x} {y}".format(x=x, y=y))
+# file = open('testOutput.txt', 'w')
+# print(x, y, file = file)
